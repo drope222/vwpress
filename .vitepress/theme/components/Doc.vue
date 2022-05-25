2781e473fb04180a30c3c7cdef4beca481a4408f
Pedro Melo <pedro.meloo@hotmail.com>
2022-05-25 14:09:13 -0300

    refactor(doc): reorganize doc template

--- .vitepress/theme/components/Doc.vue
+++ .vitepress/theme/components/Doc.vue
@@ -3,7 +3,7 @@ import DocFooter from "./DocFooter.vue"
 </script>
 <template>
   <div class="flex w-full">
-    <div class="flex-auto min-w-0 max-w-4xl lg:px-8">
+    <div class="flex-auto min-w-0 max-w-4xl lg:px-8 pb-10">
      <slot />
 
      <DocFooter />
--- .vitepress/theme/components/DocFooter.vue
+++ .vitepress/theme/components/DocFooter.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
-import { useSidebar, getActive } from "../composables/sidebar-links"
-import { WButton } from "vue-windi"
-const { sidebarLinks } = useSidebar();
+import {  getActive } from "../composables/sidebar-links";
+import { useNextAndPrevLinks } from "../composables/nextAndPrevLinks"
+import { WButton } from "vue-windi";
+import { IconEdit, IconArrowBack, IconArrowForward } from "./icons";
 
+ const { next, previous } = useNextAndPrevLinks();
 
 </script>
 <template>
-<div v-if="getActive()" class="my-8">
-<WButton variant="link" tag="a" target="_blank" :href="getActive()">
-<svg class="h-5 mr-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M2 26h28v2H2z" fill="currentColor"></path><path d="M25.4 9c.8-.8.8-2 0-2.8l-3.6-3.6c-.8-.8-2-.8-2.8 0l-15 15V24h6.4l15-15zm-5-5L24 7.6l-3 3L17.4 7l3-3zM6 22v-3.6l10-10l3.6 3.6l-10 10H6z" fill="currentColor"></path></svg>
-Suggest changes to this page
-</WButton>
-</div>
-
-
+  <div v-if="getActive">
+    <div class="py-8">
+      <WButton variant="link" tag="a" target="_blank" :href="getActive">
+      <IconEdit class="mr-2" />      
+        Suggest changes to this page
+      </WButton>
+    </div>
+    <div class="flex border-t pt-8 justify-between items-center">
+      <div class="max-w-1/2 flex items-center">
+        <WButton v-if="previous"  variant="transparent" size="sm" tag="a" :href="previous.link"
+          ><IconArrowBack class="mr-3 fill-primary-200" /> {{previous.text}}</WButton
+        >
+      </div>
+      <div  class="max-w-1/2 flex items-center">
+        <WButton v-if="next" size="sm" tag="a" :href="next.link"
+          >{{next.text}} <IconArrowForward class="ml-3 fill-white"
+        /></WButton>
+      </div>
+    </div>
+  </div>
 </template>
